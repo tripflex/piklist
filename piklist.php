aff133d859ef2db10795f458961e0f52c5f6a9d1
sbruner <sbruner@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-02 20:17:10 +0000

    version
    git-svn-id: https://plugins.svn.wordpress.org/piklist/trunk@566653 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- piklist.php
+++ piklist.php
@@ -3,7 +3,7 @@
 Plugin Name: Piklist
 Plugin URI: http://piklist.com
 Description: A Framework for Building Powerful Websites with WordPress.
-Version: 0.5.2
+Version: 0.5.0
 Author: Piklist
 Author URI: http://piklist.com
 */
