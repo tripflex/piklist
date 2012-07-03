bd03f8b4068efa006f50639bc27e582bda1f04dc
sbruner <sbruner@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-03 18:00:36 +0000

    0.5.2
    git-svn-id: http://plugins.svn.wordpress.org/piklist/tags/0.5.2@567007 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ parts/shared/admin-hide-ui.php
@@ -0,0 +1,20 @@
+
+<style type="text/css">
+
+  #adminmenuback,
+  #adminmenuwrap,
+  #wpadminbar,
+  #screen-meta-links,
+  #wphead,
+  #footer,
+  .update-nag {
+    display: none !important;
+    margin: 0 !important;
+  }
+  
+  #wpcontent {
+    margin-left: 15px !important;
+    padding-top: 0px !important;
+  }
+
+</style>
