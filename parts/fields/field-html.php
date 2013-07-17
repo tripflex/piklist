55a58b4ea10050ba938049dd6f3060e4299a6c17
sbruner <sbruner@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-17 19:22:18 +0000

    resetting trunk
    git-svn-id: http://plugins.svn.wordpress.org/piklist/trunk@742444 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- parts/fields/field-html.php
+++
@@ -1,9 +0,0 @@
-
-  <div 
-    id="<?php echo piklist_form::get_field_id($field, $scope, $index); ?>" 
-    <?php echo piklist_form::attributes_to_string($attributes); ?>
-  >
-
-    <?php echo wpautop($value); ?>
-    
-  </div>
--- parts/fields/html.php
+++
@@ -1,9 +0,0 @@
-
-  <div 
-    id="<?php echo piklist_form::get_field_id($field, $scope, $index, $prefix); ?>" 
-    <?php echo piklist_form::attributes_to_string($attributes); ?>
-  >
-
-    <?php echo wpautop($value); ?>
-    
-  </div>
