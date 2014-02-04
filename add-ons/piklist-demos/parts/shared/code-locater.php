f122cf8677205d4121446dfc558bcd1f1895134d
sbruner <sbruner@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-04 19:44:20 +0000

    resetting trunk
    git-svn-id: https://plugins.svn.wordpress.org/piklist/trunk@851442 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- add-ons/piklist-demos/parts/shared/code-locater.php
+++
@@ -1,8 +0,0 @@
-
-<div class="piklist-demo-help-source">
-
-  <p>
-    <code><?php echo str_replace(ABSPATH, '', $location); ?></code>
-  </p>
-  
-</div>
--- add-ons/piklist-demos/parts/workflows/edit-header.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
-Flow: Edit Demo
-Page: post.php, post-new.php, post-edit.php, admin.php
-Post Type: piklist_demo
-Header: true
-Position: title
-*/
-?>
